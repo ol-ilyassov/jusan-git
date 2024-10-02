c3bdb1fbda6bab612c6957693255f6c093ef655e
Olzhas <ol.ilyassov@gmail.com>
2024-10-02 21:21:40 +0500

    upd: make fit task without further modifications

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-    echo "[$0]: id is required."
-    exit 1
-fi
+# Возможное улучшение кода, читать id снаружи:
+# if [ "$#" -ne 1 ]; then
+    # echo "[$0]: id is required."
+    # exit 1
+# fi
 
-in="$1"
+# in="$1"
 
-addr="https://stepik.org/api/users/$in"
+# addr="https://stepik.org/api/users/$in"
+
+addr="https://stepik.org/api/users/256"
 
 raw=$(curl -s $addr)
-echo "$raw" | jq '.users[0].first_name'
 echo "$raw" | jq '.users[0].id'
+
+# echo "$raw" | jq '.users[0].first_name'
